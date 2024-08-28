7e47124e87a76b2f49d809424d7b489581e56fff
jarbelaezestrada <julianaarbelaezestrada@gmail.com>
2024-08-28 00:28:21 -0500

    Encriptador terminado

--- index.js
+++ index.js
@@ -23,3 +23,31 @@ function encriptar() {
     alert("debes ingresar algun texto");
   }
 }
+
+function desencriptar () {
+    let texto = document.getElementById("texto").value;
+
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+
+      if (texto.length ! = 0) {
+        document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent =
+      "Texto desencriptado con exito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/green.png";
+  } else {
+    muñeco.src = "./img/detective-1424831_1280.png";
+    parrafo.textContent = "Ingresa el texto que deseas desencriptar "
+    alert("debes ingresar algun texto");
+  }
+
+      }
+
+
+
+
